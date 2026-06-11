e4d7c168c8d0aa5316859dc66fbf2bac53b697dd
brotcon26 <brotcon26@gmail.com>
2026-06-11 06:38:53 +0000

    added toml and removed btrfs from qcowbuild.sh

--- qcowbuild.sh
+++ qcowbuild.sh
@@ -4,9 +4,9 @@ sudo podman run \
     --privileged \
     --pull=newer \
     --security-opt label=type:unconfined_t \
+    -v ./config.toml:/config.toml:ro \
     -v ./output:/output \
     -v /var/lib/containers/storage:/var/lib/containers/storage \
     quay.io/centos-bootc/bootc-image-builder:latest \
     --type qcow2 \
-    --rootfs btrfs \
     registry.lab/ithilien:staging
